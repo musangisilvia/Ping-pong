07447d78028f8ae0c273cf5c00b7503df3845184
musangisilvia <musangisilvia@gmail.com>
2017-05-05 16:43:34 +0300

    add the specs to thr readme and links to deployed site and repository

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,13 @@ var pingPong = function(number) {
 
     for (var i = 1; i <= number; i++) {
         if (i % 15 === 0) {
-            $("#result").append("<li>" + "pingpong" + "</li>");
+            $("#result").append("<li class='out'>" + "pingpong" + "</li>");
         } else if (i % 5 === 0) {
-            $("#result").append("<li>" + "pong" + "</li>");
+            $("#result").append("<li class='out'>" + "pong" + "</li>");
         } else if (i % 3 === 0) {
-            $("#result").append("<li>" + "ping" + "</li>");
+            $("#result").append("<li class='out'>" + "ping" + "</li>");
         } else {
-            $("#result").append("<li>" + String(i) + "</li>");
+            $("#result").append("<li class='out'>" + String(i) + "</li>");
         }
     };
 
@@ -19,6 +19,7 @@ var pingPong = function(number) {
 $(document).ready(function() {
     $("form#pingpong").submit(function(event) {
         event.preventDefault();
+        $("li.out").remove();
         var numberIn = parseInt($("input#integer").val());
         // result = pingPong(number);
         pingPong(numberIn);
